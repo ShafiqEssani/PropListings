8fcc71698d951fe1a729d7fae5c21333bee079dd
ShafiqEssani <shafiqshamsessani@gmail.com>
2018-03-02 14:00:42 +0500

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Final Commit for submission.
 export class AppComponent {
   title = 'app';
 }
